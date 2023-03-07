1bf5e61ee893561986475334ecff02f92bf3185f
unknown <marinbarac21@gmail.com>
2023-03-07 12:03:37 +0100

    Fixing bug with mobile menu

--- next-app/src/components/Layout/Header.js
+++ next-app/src/components/Layout/Header.js
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import Logo from "../Logo/Logo";
 import Navigation from "../Navigation/Navigation";
+
+const useDisableBodyScroll = (open) => {
+    useEffect(() => {
+        if (open) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+    }, [open])
+} 
 
 const Header = ({ activeTab }) => {
     const [showNavigation, setShowNavigation] = useState(false);
@@ -14,8 +24,10 @@ const Header = ({ activeTab }) => {
         setShowNavigation(false);
     }
 
+    useDisableBodyScroll(showNavigation);
+
     return <header>
-        <Logo />
+        <div onClick={() => setShowNavigation(false)} className="logo"><Logo /></div>
         <Navigation activeTab={activeTab} show={showNavigation} closeMenu={closeMenu}/>
         <HamburgerMenu onClick={handleHamburgerClick} showNavigation={showNavigation}/>
     </header>
